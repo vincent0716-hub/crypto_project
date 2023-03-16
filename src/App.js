45f2967dc808b735eabda1ee6332ae56cdd78eae
vincent0716-hub <vincentlin968@gmail.com>
2023-03-16 18:46:09 +1100

    crypto header complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { FaTwitter,FaDiscord,FaBars} from 'react-icons/fa';
 
+import React, { useState } from 'react'
 function App() {
+  const [navbar, setNavbar] = useState(false);
+   
+    const changeBackground = () => {
+        console.log(window.scrollY)
+        if (window.scrollY >= 5) {
+            setNavbar(true)
+        } else {
+            setNavbar(false)
+        }
+    }
+    if (typeof window !== 'undefined') {
+        
+        window.addEventListener('scroll', changeBackground)
+    } else {
+        console.log('You are on the server,Cannot execute')
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <nav className={navbar?'bg-black text-[#f3f2f4] ':'bg-[#1a166e] text-[#f3f2f4]'}>
+        <div className='flex text-[20px] items-center justify-between
+        m-0 max-w-[120rem] p-12
+        font-Grotesk 
+        '>
+        <a className='text-[2rem]'>
+          COINDOM 
         </a>
-      </header>
+        <ul className='flex gap-[4rem]  max-[750px]:hidden'>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Market</a>
+          </li>
+          <li>
+            <a>Choose Us</a>
+          </li>
+          <li>
+            <a>Join</a>
+          </li>
+        </ul>
+        <span className='flex gap-[2rem] text-[2rem]'>
+          <FaTwitter className='max-[750px]:hidden'></FaTwitter>
+          <FaDiscord className='max-[750px]:hidden'></FaDiscord>
+          <FaBars className='min-[750px]:hidden'></FaBars>
+          
+        </span>
+        </div>
+      </nav>
     </div>
   );
 }
