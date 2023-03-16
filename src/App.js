bfe61529b97f83de067fac7fc3d9dce0018af268
vincent0716-hub <vincentlin968@gmail.com>
2023-03-16 20:47:46 +1100

    add hover icon

--- src/App.js
+++ src/App.js
@@ -1,57 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
-import { FaTwitter,FaDiscord,FaBars} from 'react-icons/fa';
+import { FontAwesomeIcon, FaTwitter, FaDiscord, FaBars } from 'react-icons/fa';
 
 import React, { useState } from 'react'
+import Home from './pages/home'
 function App() {
   const [navbar, setNavbar] = useState(false);
-   
-    const changeBackground = () => {
-        console.log(window.scrollY)
-        if (window.scrollY >= 5) {
-            setNavbar(true)
-        } else {
-            setNavbar(false)
-        }
-    }
-    if (typeof window !== 'undefined') {
-        
-        window.addEventListener('scroll', changeBackground)
+  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered2, setIsHovered2] = useState(false);
+  const changeBackground = () => {
+    console.log(window.scrollY)
+    if (window.scrollY >= 5) {
+      setNavbar(true)
     } else {
-        console.log('You are on the server,Cannot execute')
+      setNavbar(false)
     }
+  }
+  if (typeof window !== 'undefined') {
+
+    window.addEventListener('scroll', changeBackground)
+  } else {
+    console.log('You are on the server,Cannot execute')
+  }
   return (
     <div className="App">
-      <nav className={navbar?'bg-black text-[#f3f2f4] ':'bg-[#1a166e] text-[#f3f2f4]'}>
+      <nav className={navbar ? 'fixed bg-[black] text-[#f3f2f4] w-full' : 'fixed w-full bg-[transparent] text-[#f3f2f4]'}>
         <div className='flex text-[20px] items-center justify-between
-        m-0 max-w-[120rem] p-12
+        m-0 max-w-[100%] p-12
         font-Grotesk 
         '>
-        <a className='text-[2rem]'>
-          COINDOM 
-        </a>
-        <ul className='flex gap-[4rem]  max-[750px]:hidden'>
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>Market</a>
-          </li>
-          <li>
-            <a>Choose Us</a>
-          </li>
-          <li>
-            <a>Join</a>
-          </li>
-        </ul>
-        <span className='flex gap-[2rem] text-[2rem]'>
-          <FaTwitter className='max-[750px]:hidden'></FaTwitter>
-          <FaDiscord className='max-[750px]:hidden'></FaDiscord>
-          <FaBars className='min-[750px]:hidden'></FaBars>
-          
-        </span>
+          <a className='text-[2rem]'>
+            COINDOM
+          </a>
+          <ul className='flex gap-[4rem]  max-[750px]:hidden '>
+            <li >
+              <a className='hover:bg-clip-text hover:bg-gradient-to-tr from-[#2600fc] to-[#ff00ea] hover:text-transparent hover:transition-all'>Home</a>
+            </li>
+            <li>
+              <a className='hover:bg-clip-text hover:bg-gradient-to-tr from-[#2600fc] to-[#ff00ea] hover:text-transparent hover:transition-all'>Market</a>
+            </li>
+            <li>
+              <a className='hover:bg-clip-text hover:bg-gradient-to-tr from-[#2600fc] to-[#ff00ea] hover:text-transparent hover:transition-all'>Choose Us</a>
+            </li>
+            <li>
+              <a className='hover:bg-clip-text hover:bg-gradient-to-tr from-[#2600fc] to-[#ff00ea] hover:text-transparent hover:transition-all'>Join</a>
+            </li>
+          </ul>
+          {/* cursor-pointer:改變鼠標當鼠標指向特定物件時 */}
+          <span className='flex gap-[2rem] text-[2rem] '>
+            <FaTwitter className='cursor-pointer max-[750px]:hidden'
+             onMouseEnter={() => setIsHovered2(true)}
+             onMouseLeave={() => setIsHovered2(false)}
+             style={{ color: isHovered2 ? '#ff00ea' : '',transition:'color 0.5s ease'}}></FaTwitter>
+            <FaDiscord 
+            className='cursor-pointer max-[750px]:hidden'
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+              style={{ color: isHovered ? '#ff00ea' : '',transition:'color 0.5s ease'}}></FaDiscord>
+            <FaBars className='min-[750px]:hidden'></FaBars>
+
+          </span>
+
         </div>
       </nav>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <section className='bg-gradient-to-t from-[#0f051d] to-[#130749] w-full h-[2000px]'>
+
+    </section>
+  )
+}
+
+export default home
